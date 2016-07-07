c686de37896d19ef35010ee39bb7c7fcdd27a8ca
Dorian Wouters <elementw@openmailbox.org>
2016-07-07 10:38:16 +0200

    GL FeatureSupport: extend VAO detection to OES extension

--- src/render/gl/FeatureSupport.cpp
+++ src/render/gl/FeatureSupport.cpp
@@ -9,7 +9,8 @@ using F = FeatureSupport;
 bool F::VAO;
 
 void F::probe() {
-  VAO = epoxy_has_gl_extension("GL_ARB_vertex_array_object");
+  VAO = epoxy_has_gl_extension("GL_ARB_vertex_array_object") or
+        epoxy_has_gl_extension("GL_OES_vertex_array_object");
 }
 
 }
